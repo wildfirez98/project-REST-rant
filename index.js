d716b2dfed7ae05219b33218a5bf9d11a4871b0e
Jamie <james@areafamily.us>
2022-02-22 10:10:52 -0600

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const express = require('express');
-const app = express();
+require('dotenv').config(); // Initiate .env file for environment variables
+const express = require('express'); // Instantiate express
+const app = express(); // Assign express constructor to a variable
 
+// GET Endpoint for '/' or homepage
 app.get('/', (req, res) => {
     res.send("Hello World")
 });
 
-app.listen(3000, console.log("I'm awake!"));
+// GET Wildcard route - MUST be at end of the list of endpoints so it doesn't override other endpoints
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page Not Found</h1>') // Insert <h1> tag with 404 text if endpoint is not valid and display on page
+});
+
+// Keep server socket open with listening to a PORT from the .env file and log to console "I'm awake" so we know the server is open
+app.listen(process.env.PORT, console.log("I'm awake!"));
 
