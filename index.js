f0f5455fce670fe344d5d4acb0fa86420c4b3a76
Jamie <james@areafamily.us>
2022-02-22 10:25:45 -0600

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config(); // Initiate .env file for environment variables
 const express = require('express'); // Instantiate express
 const app = express(); // Assign express constructor to a variable
 
+app.use('/places', require('./controllers/places')); // Set all routes in the places controller relative to /places. Means that /places will be added in front of all endpoints we define in the controller! 
+                                                    
 // GET Endpoint for '/' or homepage
 app.get('/', (req, res) => {
     res.send("Hello World")
