7e68ea6198d11b0f51ce1ee2a8e4cb65f8c34bb9
Jamie <james@areafamily.us>
2022-02-27 12:28:18 -0600

    FORM data now going thru and new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,31 +1,29 @@
 const router = require('express').Router(); //Instantiate router contoller thru express 
+const places = require('../models/places.js')
 
 // GET - /places endpoint
 router.get('/', (req,res) => { // First route - GET route that will eventually show a list of all places
-    let places = [{
-        name: 'J-Gilberts',
-        city: 'Overland Park',
-        state: 'KS',
-        cuisines: 'Steak, Seafood',
-        pic: 'images/steak.jpg'
-      }, {
-        name: 'Italian Delight',
-        city: 'Kansas City',
-        state: 'KS',
-        cuisines: 'Pizza, Pasta',
-        pic: 'images/italian.jpg'
-      }]
     res.render('places/index', {places})
 });
 
 // POST - /places - Create new place endpoint
 router.post('/', (req, res) => {
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 // GET - /places/new endpoint
 router.get('/new', (req, res) => {
-  console.log(req.body)
   res.render('places/new')
 })
 
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'J-Gilberts',
+    city: 'Overland Park',
+    state: 'KS',
+    cuisines: 'Steak, Seafood',
+    pic: 'images/steak.jpg'
+  }, {
+    name: 'Italian Delight',
+    city: 'Kansas City',
+    state: 'KS',
+    cuisines: 'Pizza, Pasta',
+    pic: 'images/italian.jpg'
+  }]
