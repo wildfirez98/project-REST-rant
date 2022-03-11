cb270e22c9309a4578338693242dfadadf663f74
Jamie <james@areafamily.us>
2022-03-10 18:31:34 -0600

    Comments working!

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -34,6 +34,30 @@ function show ({place}) {
             <p>Located in {place.city}, {place.state}</p>
             <h1>Comments</h1>
             <p>{comments}</p>
+            <h2>Got Your Own Rant or Rave?</h2>
+            <form action={`/places/${place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control" />
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>
             <a href={`/places/${place.id}/edit`} className="btn btn-warning">Edit</a>
             <form method="POST" action={`/places/${place.id}?_method=DELETE`}>
                 <button type="submit" className="btn btn-danger">Delete</button>
